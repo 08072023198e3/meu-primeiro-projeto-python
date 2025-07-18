97cc0c9f21d19772841164cc13e30e80662bb5eb
08072023198e3 <marimatiasrodrigues28@gmail.com>
2025-07-18 00:04:47 -0300

    Update saudacao.py

--- saudacao.py
+++ saudacao.py
@@ -1,5 +1,6 @@
-def saudacao(nome) :
-return f"Olá, {nome}! Bem-vinda ao seu primeiro projeto Python no GitHub!"
- if __nome__ == "__main__":  
-nome_usuario = input("mari")
-   print(saudacao(nome_usuario))
+def saudacao(nome):
+    return f"Olá, {nome}! Bem-vinda ao seu primeiro projeto Python no GitHub!"
+
+if __name__ == "__main__":
+    nome_usuario = input("Digite seu nome: ")
+    print(saudacao(nome_usuario))
