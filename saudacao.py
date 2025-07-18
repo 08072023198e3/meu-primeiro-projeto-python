01809e1c930bbc4ee0989a24fbfb9e1934a4834c
08072023198e3 <marimatiasrodrigues28@gmail.com>
2025-07-17 23:22:48 -0300

    Create saudacao.py

---
+++ saudacao.py
@@ -0,0 +1,5 @@
+def saudacao(nome) :
+return f"Olá, {nome}! Bem-vinda ao seu primeiro projeto Python no GitHub!"
+ if __nome__ == "__main__":  
+nome_usuario = input("mari")
+   print(saudacao(nome_usuario))
